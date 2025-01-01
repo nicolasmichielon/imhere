014be8565254e3aa37bbfd7509a245010d3d30a2
nicolasmichielon <nicolasmichielon@gmail.com>
2025-01-01 12:16:39 -0300

    add StatusBar component to App for improved UI consistency

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
+import { StatusBar } from "react-native";
 import { Home } from "./src/screens/Home";
+import React from "react";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" />
+      <Home />
+    </>
+  );
 }
